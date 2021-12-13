fa9ec455bad960b7fabb0f894751fc32d7ddbc5c
Pang Shangzhi <158505862@qq.com>
2021-12-13 15:31:23 +0800

    project1b

--- proj1b/TestOffByOne.java
+++ proj1b/TestOffByOne.java
@@ -13,9 +13,9 @@ public class TestOffByOne {
         assertTrue(offByOne.equalChars('a', 'b'));
         assertTrue(offByOne.equalChars('r', 'q'));
         assertTrue(offByOne.equalChars('&', '%'));
+        assertFalse(offByOne.equalChars('a', 'A'));
         assertFalse(offByOne.equalChars('a', 'e'));
         assertFalse(offByOne.equalChars('z', 'a'));
         assertFalse(offByOne.equalChars('a', 'a'));
     }
-    // Uncomment this class once you've created your CharacterComparator interface and OffByOne class. *
 }
--- proj1b/TestPalindrome.java
+++ proj1b/TestPalindrome.java
@@ -6,7 +6,7 @@ public class TestPalindrome {
     // You must use this palindrome, and not instantiate
     // new Palindromes, or the autograder might be upset.
     static Palindrome palindrome = new Palindrome();
-    static OffByOne offByOne = new OffByOne();
+    static CharacterComparator offByOne = new OffByOne();
 
     @Test
     public void testWordToDeque() {
