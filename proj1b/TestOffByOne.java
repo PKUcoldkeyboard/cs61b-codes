5216403d126c4ab3e25e4490fae996061a7dfc4c
Pang Shangzhi <158505862@qq.com>
2021-12-13 16:36:31 +0800

    project1b final

--- proj1b/TestOffByOne.java
+++ proj1b/TestOffByOne.java
@@ -13,9 +13,10 @@ public class TestOffByOne {
         assertTrue(offByOne.equalChars('a', 'b'));
         assertTrue(offByOne.equalChars('r', 'q'));
         assertTrue(offByOne.equalChars('&', '%'));
+
         assertFalse(offByOne.equalChars('a', 'A'));
         assertFalse(offByOne.equalChars('a', 'e'));
-        assertFalse(offByOne.equalChars('z', 'a'));
+        assertFalse(offByOne.equalChars('a', 'z'));
         assertFalse(offByOne.equalChars('a', 'a'));
     }
 }
